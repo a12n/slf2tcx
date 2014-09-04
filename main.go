625030000ded20c837b26dfc1a451284394df8ed
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-04 17:27:36 +0400

    main: print usage

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	// TODO
+	// var slf *Slf
+	// var gpx *Gpx
+	// var tcx *Tcx
+	switch len(os.Args) {
+	case 3:
+		// slf = new(Slf)
+		// tcx = new(Tcx)
+	case 4:
+		// slf = new(Slf)
+		// gpx = new(Gpx)
+		// tcx = new(Tcx)
+	default:
+		fmt.Fprint(os.Stderr, "Usage: ", os.Args[0], " input.slf [replace_trk.gpx] output.tcx\n")
+		os.Exit(1)
+	}
 }
