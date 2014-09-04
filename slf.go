daf65e4a1d69f6e9e48e6f6e29db29f8a5efac53
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-04 18:54:09 +0400

    slf: add GeneralInformation struct

--- slf.go
+++ slf.go
@@ -1,5 +1,10 @@
 package main
 
+type GeneralInformation struct {
+	SerialNumber string `xml:"serialNumber,attr"`
+	Unit string `xml:"unit,attr"`
+}
+
 type Log struct {
 	Revision int `xml:"revision,attr"`
 	// TODO
