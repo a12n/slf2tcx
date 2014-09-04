e436ece50b75d1a908332078e47370b26b193156
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-05 00:45:38 +0400

    tcx: add Save() and Add{Slf,Gpx}()

--- src/tcx/tcx.go
+++ src/tcx/tcx.go
@@ -1,5 +1,24 @@
 package tcx
 
+import (
+	"gpx"
+	"slf"
+)
+
 type TrainingCenterDatabase struct {
+	xmlns string `xml:"xmlns,attr"`
+}
+
+func (t *TrainingCenterDatabase) AddSlf(s *slf.Log) {
 	// TODO
+}
+
+func (t *TrainingCenterDatabase) AddGpx(s *gpx.Gpx) {
+	// TODO
+}
+
+func (t *TrainingCenterDatabase) Save(path string) error {
+	t.xmlns = "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
+	// TODO
+	return nil
 }
