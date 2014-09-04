5f96fbaa74487bc55e5e8bd6f1c0cf004a8cbaa9
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-05 00:18:41 +0400

    main.go: adjusted for pkgs

--- src/slf2tcx/main.go
+++ src/slf2tcx/main.go
@@ -3,40 +3,55 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"gpx"
 	"log"
 	"os"
+	"slf"
+	"sort"
+	"tcx"
 )
 
-func decodeFile(name string, v interface{}) {
+func Load(path string, ans interface{}) {
 	var err error
 	var file *os.File
-	if file, err = os.Open(name); err != nil {
+	if file, err = os.Open(path); err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
-	if err = xml.NewDecoder(file).Decode(v); err != nil {
+	if err = xml.NewDecoder(file).Decode(ans); err != nil {
 		log.Fatal(err)
 	}
 }
 
+func LoadSlf(path string) *slf.Log {
+	ans := new(slf.Log)
+	Load(path, ans)
+	sort.Sort(slf.LogEntryArray(ans.LogEntries.LogEntry))
+	sort.Sort(slf.MarkerArray(ans.Markers.Marker))
+	return ans
+}
+
+func LoadGpx(path string) *gpx.Gpx {
+	ans := new(gpx.Gpx)
+	Load(path, ans)
+	return ans
+}
+
 func main() {
-	var slf *Log
-	var gpx *Gpx
-	var tcx *Tcx
+	var slf *slf.Log
+	var gpx *gpx.Gpx
+	var tcx *tcx.TrainingCenterDatabase = new(tcx.TrainingCenterDatabase)
 	switch len(os.Args) {
 	case 3:
-		tcx = new(Tcx)
+		slf = LoadSlf(os.Args[1])
 	case 4:
-		gpx = new(Gpx)
-		decodeFile(os.Args[2], gpx)
-		tcx = new(Tcx)
+		slf = LoadSlf(os.Args[1])
+		gpx = LoadGpx(os.Args[2])
 	default:
 		fmt.Fprint(os.Stderr, "Usage: ", os.Args[0], " input.slf [replace_trk.gpx] output.tcx\n")
 		os.Exit(1)
 	}
-	slf = new(Log)
-	decodeFile(os.Args[1], slf)
-	fmt.Printf("slf: %+v\n", slf)
-	fmt.Printf("gpx: %+v\n", gpx)
-	fmt.Printf("tcx: %+v\n", tcx)
+	fmt.Printf("slf: %#v\n", slf)
+	fmt.Printf("gpx: %#v\n", gpx)
+	fmt.Printf("tcx: %#v\n", tcx)
 }
