a31098f9020cbaefa4f994719fac0bee062bd10a
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-04 19:10:17 +0400

    slf: add LogEntry, use it in LogValues

--- slf.go
+++ slf.go
@@ -9,7 +9,32 @@ type MetaInformation struct {
 	// TODO
 }
 
+type LogEntry struct {
+	IsPause boolean `xml:"IsPause"`
+	PauseTime int `xml:"PauseTime"`
+	IsWaypoint boolean `xml:"IsWaypoint"`
+	Title string `xml:"Title"`
+	Description string `xml:"Description"`
+	Rotations int `xml:"Rotations"`
+	RelativeRotations int `xml:"RelativeRotations"`
+	Speed double `xml:"Speed"`
+	Heartrate int `xml:"Heartrate"`
+	Altitude int `xml:"Altitude"`
+	Temperature int `xml:"Temperature"`
+	RideTime double `xml:"RideTime"`
+	Distance double `xml:"Distance"`
+	Incline double `xml:"Incline"`
+	RiseRate double `xml:"RiseRate"`
+	DistanceDownhill double `xml:"DistanceDownhill"`
+	DistanceUphill double `xml:"DistanceUphill"`
+	AltitudeDifferenceDownhill double `xml:"AltitudeDifferenceDownhill"`
+	AltitudeDifferenceUphill double `xml:"AltitudeDifferenceUphill"`
+	RideTimeUphill double `xml:"RideTimeUphill"`
+	RideTimeDownhill double `xml:"RideTimeDownhill"`
+}
+
 type LogValues struct {
+	LogEntry []*LogEntry `xml:"LogEntry"`
 }
 
 type Log struct {
