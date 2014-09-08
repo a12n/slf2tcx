1178b6e4b47932d6c8f08c8be69bd7f585547651
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-08 23:17:35 +0400

    use named return values in TCX

--- src/tcx/tcx.go
+++ src/tcx/tcx.go
@@ -94,17 +94,13 @@ func (t *TrainingCenterDatabase) ReplaceTrack(track *gpx.Gpx) error {
 	return nil
 }
 
-func (t *TrainingCenterDatabase) Save(path string) error {
-	var err error
+func (t *TrainingCenterDatabase) Save(path string) (err error) {
 	var file *os.File
 	if file, err = os.Create(path); err != nil {
-		return err
+		defer file.Close()
+		var encoder *xml.Encoder = xml.NewEncoder(file)
+		encoder.Indent("", "\t")
+		err = encoder.Encode(t)
 	}
-	defer file.Close()
-	var encoder *xml.Encoder = xml.NewEncoder(file)
-	encoder.Indent("", "\t")
-	if err = encoder.Encode(t); err != nil {
-		return err
-	}
-	return nil
+	return
 }
