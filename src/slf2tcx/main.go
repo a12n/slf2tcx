fc972195d3a23ec0d532e89bd0625e7b1b40cb52
Anton Yabchinskiy <arn@devline.ru>
2014-09-08 16:35:59 +0400

    use FromLog() and ReplaceTrack()

--- src/slf2tcx/main.go
+++ src/slf2tcx/main.go
@@ -25,21 +25,25 @@ func main() {
 		fmt.Fprint(os.Stderr, "Usage: ", os.Args[0], " input.slf [replace_trk.gpx] output.tcx\n")
 		os.Exit(1)
 	}
-	var ans *tcx.TrainingCenterDatabase = new(tcx.TrainingCenterDatabase)
+	var ans *tcx.TrainingCenterDatabase
 	var track *gpx.Gpx
 	var workout *slf.Log
 	var err error
-	// Load SLF workout
+	// Load SLF workout and create TCX
 	if workout, err = slf.Load(workoutPath); err != nil {
 		log.Fatal(err)
 	}
-	ans.FromLog(workout)
+	if ans, err = tcx.FromLog(workout); err != nil {
+		log.Fatal(err)
+	}
 	// Load GPX track
 	if trackPath != "" {
 		if track, err = gpx.Load(trackPath); err != nil {
 			log.Fatal(err)
 		}
-		ans.ReplaceTrack(track)
+		if err = ans.ReplaceTrack(track); err != nil {
+			log.Fatal(err)
+		}
 	}
 	// Save TCX
 	if err = ans.Save(ansPath); err != nil {
