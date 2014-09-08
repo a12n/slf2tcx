ee09688d5d46e60de64f583bb6442bebc71cbd77
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-08 16:32:17 +0400

    do not try to save TCX with xmlns

--- src/tcx/tcx.go
+++ src/tcx/tcx.go
@@ -22,7 +22,6 @@ func (t *TrainingCenterDatabase) ReplaceTrack(track *gpx.Gpx) error {
 }
 
 func (t *TrainingCenterDatabase) Save(path string) error {
-	t.xmlns = "http://www.garmin.com/xmlschemas/TrainingCenterDatabase/v2"
 	var err error
 	var file *os.File
 	if file, err = os.Create(path); err != nil {
