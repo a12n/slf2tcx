5882d8be9d832b2d3157a67da328bd1df85906a0
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-09 17:14:30 +0400

    gpx: simpler XML encoding

--- src/gpx/gpx.go
+++ src/gpx/gpx.go
@@ -12,9 +12,9 @@ type Email struct {
 }
 
 type Person struct {
-	Name *string `xml:"name"`
-	Email *Email `xml:"email"`
-	// Link *Link `xml:"link"`
+	Name string `xml:"name,omitempty"`
+	Email *Email `xml:"email,omitempty"`
+	// Link *Link `xml:"link,omitempty"`
 }
 
 type Wpt struct {
@@ -24,54 +24,54 @@ type Wpt struct {
 	Time *time.Time `xml"time"`
 	// MagVar *float64 `xml"magvar"`
 	// GeoidHeight *float64 `xml"geoidheight"`
-	Name *string `xml:"name"`
-	Cmt *string `xml:"cmt"`
-	Desc *string `xml:"desc"`
+	Name string `xml:"name,omitempty"`
+	Cmt string `xml:"cmt,omitempty"`
+	Desc string `xml:"desc,omitempty"`
 	// Link []Link `xml:"link"`
-	Sym *string `xml:"sym"`
-	Type *string `xml:"type"`
-	// Fix *Fix `xml:"fix"`
-	// Sat *uint `xml:"sat"`
-	// Hdop *float64 `xml:"hdop"`
-	// Vdop *float64 `xml:"vdop"`
-	// Pdop *float64 `xml:"pdop"`
-	// AgeOfDgpsData *float64 `xml:"ageofdgpsdata"`
-	// DgpsId *DgpsId `xml:"dgpsid"`
+	Sym string `xml:"sym,omitempty"`
+	Type string `xml:"type,omitempty"`
+	// Fix *Fix `xml:"fix,omitempty"`
+	// Sat *uint `xml:"sat,omitempty"`
+	// Hdop *float64 `xml:"hdop,omitempty"`
+	// Vdop *float64 `xml:"vdop,omitempty"`
+	// Pdop *float64 `xml:"pdop,omitempty"`
+	// AgeOfDgpsData *float64 `xml:"ageofdgpsdata,omitempty"`
+	// DgpsId *DgpsId `xml:"dgpsid,omitempty"`
 }
 
 type TrkSeg struct {
-	TrkPt []*Wpt `xml:"trkpt"`
+	TrkPt []Wpt `xml:"trkpt,omitempty"`
 }
 
 type Trk struct {
-	// Name *string `xml:"name"`
-	// Cmt *string `xml:"cmt"`
-	// Desc *string `xml:"desc"`
-	// Src *string `xml:"src"`
-	// Link []Link `xml:"link"`
-	// Number *uint `xml:"number"`
-	// Type *string `xml:"type"`
-	TrkSeg []*TrkSeg `xml:"trkseg"`
+	// Name string `xml:"name,omitempty"`
+	// Cmt string `xml:"cmt,omitempty"`
+	// Desc string `xml:"desc,omitempty"`
+	// Src string `xml:"src,omitempty"`
+	// Link []Link `xml:"link,omitempty"`
+	// Number *uint `xml:"number,omitempty"`
+	// Type string `xml:"type,omitempty"`
+	TrkSeg []TrkSeg `xml:"trkseg,omitempty"`
 }
 
 type Metadata struct {
-	Name *string `xml:"name"`
-	Desc *string `xml:"desc"`
-	Author *Person `xml:"author"`
-	// Copyright *Copyright `xml:"copyright"`
-	// Link []Link `xml:"link"`
-	Time *time.Time `xml:"time"`
-	// Keywords *string `xml:"keywords"`
-	// Bounds *Bounds `xml:"bounds"`
+	Name string `xml:"name,omitempty"`
+	Desc string `xml:"desc,omitempty"`
+	Author *Person `xml:"author,omitempty"`
+	// Copyright *Copyright `xml:"copyright,omitempty"`
+	// Link []Link `xml:"link,omitempty"`
+	Time *time.Time `xml:"time,omitempty"`
+	// Keywords string `xml:"keywords,omitempty"`
+	// Bounds *Bounds `xml:"bounds,omitempty"`
 }
 
 type Gpx struct {
 	// Version string `xml:"version,attr"`
 	// Creator string `xml:"creator,attr"`
-	Metadata *Metadata `xml:"metadata"`
-	Wpt []*Wpt `xml:"wpt"`
-	// Rte []*Rte `xml:"rte"`
-	Trk []*Trk `xml:"trk"`
+	Metadata *Metadata `xml:"metadata,omitempty"`
+	Wpt []Wpt `xml:"wpt,omitempty"`
+	// Rte []Rte `xml:"rte,omitempty"`
+	Trk []Trk `xml:"trk,omitempty"`
 }
 
 func Load(path string) (ans *Gpx, err error) {
