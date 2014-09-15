cbe06d765bb55d5932728dd876871c5337122f14
Anton Yabchinskiy <arn@bestmx.ru>
2014-09-16 00:28:40 +0400

    dumpalt: dump all laps

--- src/dumpalt/main.go
+++ src/dumpalt/main.go
@@ -8,11 +8,13 @@ import (
 )
 
 func dump(wrk *tcx.TrainingCenterDatabase) {
-	for nTrkpt, trkpt := range wrk.Activity[0].Lap[0].Track[0].Trackpoint {
-		if trkpt.Altitude != nil {
-			fmt.Printf("%d\t%f\n", trkpt.Time.Unix(), *trkpt.Altitude)
-		} else {
-			log.Printf("Trackpoint %d has nil altitude\n", nTrkpt)
+	for _, lap := range wrk.Activity[0].Lap {
+		for nTrkpt, trkpt := range lap.Track[0].Trackpoint {
+			if trkpt.Altitude != nil {
+				fmt.Printf("%d\t%f\n", trkpt.Time.Unix(), *trkpt.Altitude)
+			} else {
+				log.Printf("Trackpoint %d has nil altitude\n", nTrkpt)
+			}
 		}
 	}
 }
